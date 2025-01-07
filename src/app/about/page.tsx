afc69cd974f1b068855a99343614f51fd8103341
Carolina Venegas <carolinafran.vs@gmail.com>
2025-01-07 18:28:44 -0300

    feat:Proyectando el esquema de a futuro del proyecto, con el navbar y body, solo footer esta pendiente.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+// pages/about.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>Sobre mí</h1>
+      <p>¡Hola! Soy [tu nombre], un desarrollador apasionado...</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+// pages/contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Acerca de nosotros</h1>
+      <p>Esta es la página de información sobre nosotros.</p>
+    </div>
+  );
+};
+export default Contact;
